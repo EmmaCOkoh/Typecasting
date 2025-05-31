e3efe1a73b2dbcd3ee0cc36ec6ddb6abfcba7fea
Emmanuel Chuks Okoh <okoh.ce@gmail.com>
2025-05-31 12:42:32 -0400

    Add files via upload

---
+++ Typecasting1.py
@@ -0,0 +1,30 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "Emmanuel Chuks Okoh"
+age = 29
+gpa = 5.6
+is_student = False
+
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+# int to float
+age = float(age)
+print(age)
+print(type(age))
+
+# int to string
+age = str(age)
+print(age)
+print(type(age))
+
+# float to int
+gpa = int(gpa)
+print(gpa)
+print(type(gpa))
+
+
